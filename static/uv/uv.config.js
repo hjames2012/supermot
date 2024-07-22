d632895c1fcffb1f650c1d3a4637d07167e4870d
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-07-22 20:59:47 +0000

    W riz

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare:'https://1234nockingondador.vercel.app/',
+    bare:'https://k.kdot.cam/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
