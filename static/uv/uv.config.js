070de3d855ac4dd68b7fe882ab60fd1a7ebf2c50
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-10-24 15:05:37 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare: 'https://interstellar-v6-ten.vercel.app/ov/',
+    bare: 'https://abyss-web-intel.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
