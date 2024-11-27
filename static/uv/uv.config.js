83beaf2897c9e3a422880555d9274814d86d883e
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-11-27 14:24:53 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare: 'https://ihatetinfoil.us.to/bare/',
+    bare: 'http://ihatetinfoil.us.to/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
