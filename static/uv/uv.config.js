b9b241421865cf338c9aa9bd4e34bfbf9996b839
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-11-27 23:05:36 +0000

    what

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare: 'https://itsnotrightorwrong.us.to/bare/',
+    bare: 'http://tinfoilisbad.us.to/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
