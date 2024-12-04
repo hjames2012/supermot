e8c4d2ceed03ef1334b53d49042826cf2c00a365
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-12-04 08:05:20 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare: 'https://v2202411241114295257.megasrv.de/bare/',
+    bare: '',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
