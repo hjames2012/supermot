01b4039986c040f78f0fd8e822b838a82d12e569
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2024-12-04 08:32:40 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/jmwsw/',
-    bare: '',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+  prefix: '/static/jmwsw/',
+  bare: 'https://introducing.wine-software.com/bare/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/static/uv/uv.handler.js',
+  bundle: '/static/uv/uv.bundle.js',
+  config: '/static/uv/uv.config.js',
+  sw: '/static/uv/uv.sw.js',
 };
