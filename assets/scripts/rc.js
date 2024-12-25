92fec6ae582ff9bd8b07404d134ab7c44772b8a9
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-12-24 18:25:49 -0800

    Update rc.js

--- assets/scripts/rc.js
+++ assets/scripts/rc.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   aboutBlank.style.cursor = "pointer";
   aboutBlank.style.color = "var(--txtcolor)";
   aboutBlank.onclick = function () {
-    buffedAB();
+    aboutBlankFunction();
     contextMenu.style.display = "none";
   };
 
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   buffedBlank.style.cursor = "pointer";
   buffedBlank.style.color = "var(--txtcolor)";
   buffedBlank.onclick = function () {
-    aboutBlankFunction();
+    buffedAB();
     contextMenu.style.display = "none";
   };
 
