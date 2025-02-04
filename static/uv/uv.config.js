75f25a8b901ad306bf9ca52fe608cd3e214867b8
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2025-02-04 07:46:09 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jmwsw/',
-  bare: 'https://frog.vpn.net.br/bare/',
+  bare: 'https://bare.vpn.net.br/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/uv/uv.handler.js',
