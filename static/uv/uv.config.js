f4422e8aabbe72b622b89c206bce7d4365890407
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2025-02-04 08:51:39 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jmwsw/',
-  bare: 'https://bare.vpn.net.br/bare/',
+  bare: 'https://introducing.wine-software.com/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/uv/uv.handler.js',
