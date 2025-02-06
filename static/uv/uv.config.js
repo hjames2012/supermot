08f507c7b7fa5053f4a04dadf068243c31d112d2
Jordan <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2025-02-06 12:34:01 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jmwsw/',
-  bare: 'https://historicalfiction.vpn.net.br/bare/',
+  bare: 'https://frog.tracbras.com.br/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/uv/uv.handler.js',
