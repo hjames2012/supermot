6ed2d96e9a8d4345d1faae7110f1f1e57c90e2f2
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2026-01-30 17:41:20 -0800

    Update jmw.config.js

--- static/jmw/jmw.config.js
+++ static/jmw/jmw.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jordansmath/work/',
-  bare: 'https://larp.foundation/tspmo/',
+  bare: 'https://bare.calculra.store',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/jmw/jmw.handler.js',
