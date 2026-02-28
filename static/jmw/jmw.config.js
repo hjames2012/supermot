5a566ed9f4c0cdc340f8e45aae3b1e888780a7bf
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2026-02-27 20:37:26 -0800

    Update jmw.config.js

--- static/jmw/jmw.config.js
+++ static/jmw/jmw.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jordansmath/work/',
-  bare: 'https://lightspeeddontblock.b-cdn.net/tspmo/',
+  bare: 'https://dash.learnenglishinmerton.org/tspmo/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/jmw/jmw.handler.js',
